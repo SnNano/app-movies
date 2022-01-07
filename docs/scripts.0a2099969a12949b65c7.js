1eb6ec4949da35eeaaef853146085292a711ec59
SnNano <elmourabiti.nadia@gmail.com>
2022-01-07 12:58:46 +0100

    building gpages

--- docs/scripts.0a2099969a12949b65c7.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",e=>{const t=document.querySelector(".btn-search"),c=document.querySelector(".search-form");t.addEventListener("click",()=>{c.classList.toggle("active")})});
